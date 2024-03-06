0713940de8c8f99c8c6ad81af1d312620670d1a7
Mustafa Basel <83069151+mbaseI@users.noreply.github.com>
2024-03-06 14:58:28 +0300

    İnitial Commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+    <div>Store</div>
+}
+
+export default HomePage
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}
+        </body>
+    </html>
+  );
+}
