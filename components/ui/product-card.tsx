242289c73d5389da55cadc0698a0738bc4cd9e4e
Mustafa Basel <83069151+mbaseI@users.noreply.github.com>
2024-03-13 19:09:47 +0300

    *Headless ui added. *Product page added *Gallery and ProductCard component added

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,14 +7,22 @@ import { Product } from "@/types"
 import { Expand, ShoppingCart } from "lucide-react"
 
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 interface ProductCard {
   data: Product
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter()
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`)
+  }
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           className="aspect-square object-cover rounded-md"
